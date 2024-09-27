33436a6ac198f973b7c76405c9d95e3331f152c4
Kael <johnmark.cuyos@evsu.edu.ph>
2024-09-27 22:43:11 +0800

    crop image

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+    aspectRatio: 0,
+    viewMode: 0,
+});
+
+document.getElementById('cropImgbtn').addEventListener('click', function() {
+    var cropImg = cropper.getCroppedCanvas().toDataURL("image/png");
+    document.getElementById('outputImg').src = cropImg;
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+### npm install cropperjs
+### npm install bootstrap
